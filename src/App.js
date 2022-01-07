f92871d451f2bf9854ebc3cccaf6dd74818d803e
ibtihajAmin <ibtihaj.amin18@gmail.com>
2022-01-07 18:40:31 +0600

    User Table is added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import UserInfoTable from './Components/UserInfoTable/UserInfoTable';
 
 function App() {
+  const usersData = [
+    { id: 1, name: 'Akash', username: 'asif_karim' },
+    { id: 2, name: 'Shohan', username: 'dreamBoy' },
+    { id: 3, name: 'Ashraful', username: 'Hunter' },
+    { id: 4, name: 'Saad', username: 'chotoSaad' }
+  ]
+
+  const [users,setUsers] = useState(usersData);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mt-3">
+      <h2>CRUD app with react</h2>
+      <div class="row align-items-start">
+    <div class="col mt-2">
+      <h2>Add user</h2>
+    </div>
+    <div class="col mt-2">
+      <h2>View user</h2>
+      <UserInfoTable users={users}></UserInfoTable>
+    </div>
+ 
+  </div>
     </div>
   );
 }
---
+++ src/Components/UserInfoTable/UserInfoTable.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const UserInfoTable = (props) => {
+    return (
+        <div>
+            <table class="table table-hover">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+      {
+        props.users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.name}</td>
+            <td>{user.username}</td>
+            <td>
+            <button type="button" class="btn btn-outline-secondary me-2">Edit</button>
+              <button type="button" class="btn btn-outline-danger">Delete</button>
+            </td>
+          </tr>
+        ))
+      }
+    </tbody>
+      </table>
+        </div>
+    );
+};
+
+export default UserInfoTable;
